e74abb6d101ec243e8f3c1ab9268ed16c14328f8
Lubna-Tnit <thenodeituae8@gmail.com>
2025-06-20 19:09:59 +0400

    Deleted

--- Backend/Tnit/src/main/java/com/example/Tnit/repository/LoginRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.Tnit.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.Tnit.model.Login;
-
-public interface LoginRepository extends JpaRepository<Login, Long> {
-	 Login findByEmail(String email);
-}
